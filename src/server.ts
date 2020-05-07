4866070da823fab29501e8335a399ef766572c38
Lucas Nogueira <lucas_snogueira@hotmail.com>
2020-05-07 12:26:14 -0300

    fix(error-handler): insert express-async-errors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 
 import routes from './routes';
 import uploadConfig from './config/upload';
