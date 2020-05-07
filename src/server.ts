5f934c9aab9a3c32895097745c71faa79a24f66c
Lucas Nogueira <lucas_snogueira@hotmail.com>
2020-05-07 12:24:54 -0300

    feat(error-handler): create error handler

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import 'reflect-metadata';
 
-import express from 'express';
-import routes from './routes';
+import express, { Request, Response, NextFunction } from 'express';
 
+import routes from './routes';
 import uploadConfig from './config/upload';
+import AppError from './errors/AppError';
 
 import './database';
 
@@ -13,6 +14,22 @@ const port = 3333;
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(port, () => {
   console.log(`🚀🤘 Server listen on port ${port}!`);
